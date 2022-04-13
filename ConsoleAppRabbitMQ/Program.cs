e9c2cd01e0faefa60d6cab60b635e97390f6ed58
mrelectronst <fhkaraman@outlook.com>
2022-04-13 16:05:55 +0300

    Add project files.

---
+++ ConsoleAppRabbitMQ/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri(""); //write AMQP URL
+
+string messageRead;
+
+do
+{
+    Console.Write("Please write your message for 50 times: ");
+    messageRead = Console.ReadLine();
+    if (messageRead != string.Empty && messageRead != null)
+    {
+        PublishMessage(messageRead);
+    }
+    else
+    {
+        Environment.Exit(0);
+    }
+
+} while (messageRead != null);
+
+void PublishMessage(string message)
+{
+    try
+    {
+        using (var connection = factory.CreateConnection())
+        {
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("exchange-fanout-logs",durable:true,type:ExchangeType.Fanout);
+
+            Enumerable.Range(0, 40).ToList().ForEach(x =>
+            {
+                var messageBody = Encoding.UTF8.GetBytes(message+$"_{x}");
+
+                channel.BasicPublish("exchange-fanout-logs","", null, messageBody);
+
+                Console.WriteLine($"'{message}-{x}' is sended");
+            });
+
+            Console.WriteLine("All messages are sended");
+        }
+    }
+    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+
+}
+
+
---
+++ SubscriberRabbitMQ/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri(""); //write AMQP URL
+
+using (var connection = factory.CreateConnection())
+{
+    var channel = connection.CreateModel();
+
+    //channel.ExchangeDeclare("exchange-fanout-logs", durable: true, type: ExchangeType.Fanout);
+
+    var rndQueuename = channel.QueueDeclare().QueueName;
+
+    channel.QueueBind(rndQueuename, "exchange-fanout-logs", "", null);
+
+    channel.BasicQos(0,1,false);
+
+    var subscriber = new EventingBasicConsumer(channel);
+
+    channel.BasicConsume(rndQueuename, false, subscriber);
+
+    Console.WriteLine("Listening...");
+
+    subscriber.Received += (object? sender, BasicDeliverEventArgs e) =>
+    {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+        Thread.Sleep(500);
+
+        Console.WriteLine($"Received Message : {message}");
+
+        channel.BasicAck(e.DeliveryTag, false);
+    };
+
+    Console.ReadLine();
+}
